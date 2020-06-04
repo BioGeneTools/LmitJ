4a0c3aeff6dac81768e3f09c0c6e18b1c1889b5c
Hezha <hezha.hassan@hezhalab.com>
2020-06-04 14:28:20 +0200

    documentation

--- blueprint/v1/crawl.py
+++ blueprint/v1/crawl.py
@@ -1,17 +1,24 @@
+"""
+Crawling script
+"""
+
+import re
 import requests
 from bs4 import BeautifulSoup
 
+
 class VocabularCollector():
     """Receives an article link and returns a dict of words and other parameters
-    
+
     Args:
         journal_name (str): The journal name.
         journal_link (str): the link of the article to be scraped.
-    
+
     Returns:
         A list of dictionaries e.g. {"word":"", "sentence:""}
     """
     newVoc = []
+
     def __init__(self, data):
         self.name = data['journal_name']
         self.link = data['journal_link']
@@ -19,62 +26,67 @@ class VocabularCollector():
     @property
     def crawl(self):
         """Checks which journal the links belongs."""
-        page = self.requests_object(self.link)
-        if isinstance(page, dict): # if requests failed, returns a dictionary of error detail rather than requests object
+        page = VocabularCollector.requests_object(self.link)
+        if isinstance(page, dict):  # if req failed, returns a dict of error
             return page
         soup = BeautifulSoup(page.content, 'html.parser')
-        return self.build_Dictionary(soup)
+        return self.build_dictionary(soup)
 
-    def build_Dictionary(self, soup):
+    def build_dictionary(self, soup):
         """To scrape articles from Spiegel journal
-        
+
         Args:
-            soup (obj): A beautifulsoup object. 
+            soup (obj): A beautifulsoup object.
         """
         _p_texts = []
         _words_list = []
-        _wordsToBeSkipped = ["_localized"]
         _refined = []
-        
+
         if self.name == "Spiegel":
-            import re
             regex = re.compile('RichText RichText--iconLinks')
-            for div in soup.find_all("div", {"class": regex}): _p_texts.append(div.p.text.replace('\n','').strip())  
+            for div in soup.find_all("div", {"class": regex}):
+                _p_texts.append(div.p.text.replace('\n', '').strip())
         elif self.name == "Tagesschau":
             for div in soup.find_all("div", class_="mod modA modParagraph"):
-                for p in div.find_all("p", class_="text small"): _p_texts.append(p.text.replace('\n','').strip())
-        else: return {"error": "provided journal is not known"}
+                for p in div.find_all("p", class_="text small"):
+                    _p_texts.append(p.text.replace('\n', '').strip())
+        else:
+            return {"error": "provided journal is not known"}
 
         for p_text in _p_texts:
             p_text_list = p_text.split(" ")
-            for index, eachWord in enumerate(p_text_list):
-                for char in '".,()-„': eachWord=eachWord.replace(char, '')
-                eachWord = eachWord.strip()
-                if eachWord and len(eachWord)>1 and eachWord not in _words_list and not eachWord.isnumeric() and not any(x in "_@#$%&*?/" for x in eachWord):
-                    _refined.append({"word": eachWord, "sentence": self.extractSentence(index, p_text_list)})
-        
+            for index, each_word in enumerate(p_text_list):
+                for char in '".,()-„':
+                    each_word = each_word.replace(char, '')
+                each_word = each_word.strip()
+                if each_word and len(each_word) > 1 and each_word not in _words_list and not each_word.isnumeric() and not any(x in "_@#$%&*?/" for x in each_word):
+                    _refined.append({"word": each_word, "sentence": VocabularCollector.extract_sentence(index, p_text_list)})
+
         return _refined
-        
-    def extractSentence(self, index, p_tags):
+
+    @staticmethod
+    def extract_sentence(index, p_tags):
         """To re-construct the sentence that a word belongs."""
         p_length = len(p_tags)
-        start = index-3; end = index+4
+        start = index-3
+        end = index+4
         if start < 0:
             start = 0
         if end > p_length:
             end = p_length
-        return ' '.join(p_tags[start:end]) 
+        return ' '.join(p_tags[start:end])
 
-    def requests_object(self, link):
+    @staticmethod
+    def requests_object(link):
         """Gets the link content through bs4 and returns an object"""
         try:
             page = requests.get(f"{link}")
         except requests.exceptions.HTTPError as err:
-            return {"error":"Http"}
+            return {"error": "Http"}
         except requests.exceptions.ConnectionError as err:
-            return {"error":"Connection"}
+            return {"error": "Connection"}
         except requests.exceptions.Timeout as err:
-            return {"error":"Timeout"}
+            return {"error": "Timeout"}
         except requests.exceptions.RequestException as err:
-            return {"error":"RequestException"}
+            return {"error": "RequestException"}
         return page
--- models.py
+++ models.py
@@ -1,27 +1,52 @@
+"""The model classes can be found here."""
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    """The User model.
+
+    Args:
+        username: username
+        password: password of the user
+    """
     __tablename__ = "User"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50))
     password = db.Column(db.String(50))
-    vocabularies = db.relationship('Vocabularies', backref='voc')
+    vocabularies = db.relationship('Vocabularies', backref='voc', cascade='all, delete-orphan')
 
     def store_in_db(self):
+        """Storing the current session in the db"""
         db.session.add(self)
         db.session.commit()
-    
+
     def delete_from_db(self):
+        """Deleting the current session in the db"""
         db.session.delete(self)
         db.session.commit()
 
     @classmethod
     def query_by_username(cls, username):
+        """Queries by username"""
         return cls.query.filter_by(username=username).first()
+    
+    @classmethod
+    def query_by_id(cls, id):
+        """Queries by id"""
+        return cls.query.filter_by(id=id).first()
+
 
 class Vocabularies(db.Model):
+    """The Vocabularies model
+
+    Args:
+        word
+        sentence
+        voc_id
+    """
     __tablename__ = "Vocabularies"
     id = db.Column(db.Integer, primary_key=True)
     word = db.Column(db.String(50))
@@ -29,26 +54,39 @@ class Vocabularies(db.Model):
     voc_id = db.Column(db.Integer, db.ForeignKey('User.id'))
 
     def store_in_db(self):
+        """Storing the current session in the db"""
         db.session.add(self)
         db.session.commit()
-    
+
     def delete_from_db(self):
+        """Deleting the current session in the db"""
         db.session.delete(self)
         db.session.commit()
 
-    def find_by_word(self, word):
-        return self.query.filter_by(word=word).first()
+    @classmethod
+    def find_by_word(cls, word):
+        """Finds a word in the dictionary"""
+        return cls.query.filter_by(word=word).first()
+
 
 class Journals(db.Model):
+    """The Journals model
+
+    Args:
+        name: the name of the journal
+        address: the website address of the journal
+    """
     __tablename__ = "Journals"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     address = db.Column(db.String(50))
 
     def store_in_db(self):
+        """Storing the current session in the db"""
         db.session.add(self)
         db.session.commit()
-    
+
     def delete_from_db(self):
+        """Deleting the current session in the db"""
         db.session.delete(self)
         db.session.commit()
